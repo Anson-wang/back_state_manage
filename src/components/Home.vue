b71e6d3cb6238c8e0314eecdf42d51fd4dda38d9
Anson-wang <375989120@qq.com>
2020-10-23 11:53:40 +0800

    Day2

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,15 @@
 <template>
-  <h2>Home</h2>
+  <el-button type="info" @click="logout">登出</el-button>
 </template>
 
 <script>
 export default {
+  methods: {
+    logout() {
+      window.sessionStorage.clear();
+      this.$router.push('/login');
+    }
+  }
 
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -26,4 +26,14 @@ const router = new VueRouter({
     mode: 'history'
 })
 
+//挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+    //to将要访问的路径 from代表从哪个路径跳转来，next是一个函数表示放行
+    if (to.path === '/login') return next()
+        //获取token,如果token为空直接返回登录界面
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login')
+    next()
+})
+
 export default router
